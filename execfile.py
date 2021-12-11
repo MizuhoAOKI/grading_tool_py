d91b0b6674f3ab20dc95c1ab1290397e79703de3
unknown <mizuhoaoki1998@gmail.com>
2021-12-11 20:57:08 +0900

    modify execfile

--- execfile.py
+++ execfile.py
@@ -15,7 +15,13 @@ def get_path(target_dir, extention=DEFAULT_EXTENTION, objection=""):
     for file in os.listdir(target_dir):
         base, ext = os.path.splitext(file)
         if ext == extention:
-            file_list.append(os.path.join(target_dir, file))
+            # file_list.append(os.path.join(target_dir, file)) # absolute path is sometimes too long
+            file_list.append(file)
+    if len(file_list) < 1 :
+        print(f"No {extention} file in {target_dir}")
+        readchar.readchar()
+        sys.exit()
+
     cli_selection = [
         inquirer.List(
             'selected_path',
@@ -25,12 +31,13 @@ def get_path(target_dir, extention=DEFAULT_EXTENTION, objection=""):
         )
     ]
     answer = inquirer.prompt(cli_selection)
-    print(f"Selected path : {answer['selected_path']}")
-    return answer['selected_path']
+    selected_path = os.path.join(target_dir, answer['selected_path'])
+    print(f"Selected path : {selected_path}")
+    return selected_path
 
 
 # Set query to identify which directory you choose.
-def execfile(query, root_dir="."):
+def execfile(query, root_dir=".", subdir="提出物の添付ファイル"):
     # Show query
     print(f"Searching dir using query : {query}\n")
 
@@ -39,38 +46,47 @@ def execfile(query, root_dir="."):
     filesndirs = os.listdir(root_path)
     dirs = [f for f in filesndirs if os.path.isdir(os.path.join(root_path,f))]
     target_dir_candidates = [l for l in dirs if query in l]
+    print(target_dir_candidates)
     if len(target_dir_candidates) < 1:
         print(f"Root directory is {root_path}")
         print(f"Query is {query}")
         print("No such directory was found.")
         print("Try another query.")
+        readchar.readchar()
         return False
     elif len(target_dir_candidates) > 1:
         print("Couldn't identify single target directory.")
         print("Try another query.")
+        readchar.readchar()
         return False
 
     # Found single target_dir of the specific student you selected.
-    target_dir = os.path.abspath(target_dir_candidates[0])
+    target_dir = os.path.abspath(os.path.join(os.path.join(root_dir,target_dir_candidates[0]), subdir))
     target_fn  = os.path.splitext(os.path.basename(get_path(target_dir)))[0] # get filename without extention.
     target_file = os.path.join(target_dir, target_fn)
+    print(target_dir)
+    print(target_file)
 
     print("\n##### Build the Program #####")
+    print(f"gcc {target_file}.c -o {target_file}")
     proc_build=subprocess.run(f"gcc {target_file}.c -o {target_file}", shell=True, stdout=PIPE, stderr=PIPE, text=True)
     print(proc_build.stdout)
-    print("\n##### Execution Result #####\n")
-    proc_exec=subprocess.run(f"{target_file}", shell=True, stdout=PIPE, stderr=PIPE, text=True)
+    # print("\n##### Execution Result #####\n")
+    # print(f"{target_file}")
+    # proc_exec=subprocess.run(os.path.basename(target_file), shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True, cwd=target_dir)
+    proc_exec=subprocess.run(f"start cmd.exe /k {os.path.basename(target_file)} ", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True, cwd=target_dir)
     print(proc_exec.stdout)
 
     print("\n############################\n")
-    print("Press Any Key to Exit")
-    readchar.readchar()
+    # print("Press Any Key to Exit")
+    # readchar.readchar()
 
 if __name__ == '__main__':
     args = sys.argv
     if len(args) == 2:
         execfile(args[1])
-    elif 3 <= len(args):
-        execfile(args[1], args[2])
+    elif 4 <= len(args):
+        execfile(args[1], args[2], args[3])
     else:
         print('Arguments are too short')
+        readchar.readchar()
